7c751de207a54719ce7b71e612cc6ff8dbc37481
schan <105832330+fullspringwater@users.noreply.github.com>
2022-05-24 14:24:24 +0900

    add

---
+++ app_ml.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import string
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+my_stopwords = stopwords.words('english')
+import joblib
+
+def run_ml() :
+    pass
